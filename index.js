32440832f2c36ec1be06303525ea101fd881933b
matanelvatkin <matanelvatkin@gmail.com>
2024-03-18 09:55:47 +0200

    add axios

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const axios = require('axios')
-const PORT = process.env.PORT || 5556
-const BASE_URL = process.env.BASE_URL
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+const PORT = process.env.PORT || 5556;
+const BASE_URL = process.env.BASE_URL;
+app.use(express.json());
+app.use(cors());
+axios.defaults.headers.common.Authorization = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5YWNoaW5lIiwiZXhwIjoxNzExMDQwNTQzfQ.JhlQcHbeIG2tA2DWclCJf8nxJuphcNdo-uWtzMliU_n5znGwAG1c6ASIg31r4lycjCwEBwKEGC51bfUZmQ0Fgg`;
+app.post("/api/adduser", async (req, res) => {
+  try {
+    const json = {
+      firstName: req.body.user_meta.first_name[0],
+      lastName: req.body.user_meta.last_name[0],
+      username: req.body.data.user_nickname,
+      providerName: "",
+      role: {
+        id: 3,
+        name: "CASTOUMER",
+        parentId: null,
+        parentName: null,
+        version: 0,
+      },
+      password: "thigusNkcsu123",
+      userProfile: {
+        organization: {
+          id: 70,
+          name: "N.S.O",
+          parentId: null,
+          parentName: null,
+          version: 0,
+          logo: "",
+        },
+        email: req.body.data.user_email,
+        phone: req.body.user_meta.billing_phone[0],
+        providerName: "",
+      },
+    };
+    const res = await axios.post("https://bof.profchecksys.com/user", json);
+    console.log(res);
 
-app.post('/api/adduser',async (req,res)=>{
-try {
-    console.log(req.body);
-      res.send('ok')
-} catch (error) {
-    
-}
-      
-})
-app.post('/api/addcheck',(req,res)=>{
-    console.log('create check',req.params);
-})
+    res.send("ok");
+  } catch (error) {}
+});
+app.post("/api/addcheck", (req, res) => {
+  console.log("create check", req.params);
+});
 
-app.listen(PORT,()=>{
-    console.log('listening on port 5556');
-})
+app.listen(PORT, () => {
+  console.log("listening on port 5556");
+});
