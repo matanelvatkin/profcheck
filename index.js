b3e9390c61549799085a243c36e655a21a624d84
matanelvatkin <matanelvatkin@gmail.com>
2024-03-18 10:00:50 +0200

    add axios

--- index.js
+++ index.js
@@ -10,33 +10,34 @@ app.use(cors());
 axios.defaults.headers.common.Authorization = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5YWNoaW5lIiwiZXhwIjoxNzExMDQwNTQzfQ.JhlQcHbeIG2tA2DWclCJf8nxJuphcNdo-uWtzMliU_n5znGwAG1c6ASIg31r4lycjCwEBwKEGC51bfUZmQ0Fgg`;
 app.post("/api/adduser", async (req, res) => {
   try {
-    const json = {
-      firstName: req.body.user_meta.first_name[0],
-      lastName: req.body.user_meta.last_name[0],
-      username: req.body.data.user_nickname,
-      providerName: "",
-      role: {
-        id: 3,
-        name: "CASTOUMER",
-        parentId: null,
-        parentName: null,
-        version: 0,
-      },
-      password: "thigusNkcsu123",
-      userProfile: {
-        organization: {
-          id: 70,
-          name: "N.S.O",
-          parentId: null,
-          parentName: null,
-          version: 0,
-          logo: "",
-        },
-        email: req.body.data.user_email,
-        phone: req.body.user_meta.billing_phone[0],
-        providerName: "",
-      },
-    };
+      const json = {
+          firstName: req.body.user_meta.first_name[0],
+          lastName: req.body.user_meta.last_name[0],
+          username: req.body.data.user_nickname,
+          providerName: "",
+          role: {
+              id: 3,
+              name: "CASTOUMER",
+              parentId: null,
+              parentName: null,
+              version: 0,
+            },
+            password: "thigusNkcsu123",
+            userProfile: {
+                organization: {
+                    id: 70,
+                    name: "N.S.O",
+                    parentId: null,
+                    parentName: null,
+                    version: 0,
+                    logo: "",
+                },
+                email: req.body.data.user_email,
+                phone: req.body.user_meta.billing_phone[0],
+                providerName: "",
+            },
+        };
+        console.log(json);
     const res = await axios.post("https://bof.profchecksys.com/user", json);
     console.log(res);
 
