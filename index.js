95461bb0c0f65d06adc03bfbed4a85b04ee330ab
matanelvatkin <matanelvatkin@gmail.com>
2024-03-27 19:38:24 +0200

    check new api /user

--- index.js
+++ index.js
@@ -10,44 +10,42 @@ app.use(cors());
 app.post("/api/adduser", async (req, res) => {
   try {
     const token = await axios.post("https://bof.profchecksys.com/account/signin", {username:'yachine',password:"Profcheck123!"});
-    
-      
-      console.log(token.data);
-    // const results =await axios.post(
-    //   "https://bof.profchecksys.com/account/signup",
-    //   {
-    //     "firstName": ""+req.body.user_meta.first_name[0],
-    //     "lastName": ""+req.body.user_meta.last_name[0],
-    //     "username": ""+req.body.user_meta.first_name[0],
-    //     "providerName": "",
-    //     "role": {
-    //       "name": "CUSTOMER",
-    //       "parentId": null,
-    //       "parentName": null,
-    //       "version": 0
-    //     },
-    //     "password": ""+req.body.data.user_pass,
-    //     "userProfile": {
-    //       "organization": {
-    //         "id": 70,
-    //         "name": "N.S.O",
-    //         "parentId": null,
-    //         "parentName": null,
-    //         "version": 0,
-    //         "logo": ""
-    //       },
-    //       "email": ""+req.body.data.user_email,
-    //       "phone": ""+req.body.user_meta.billing_phone[0],
-    //       "providerName": ""
-    //     }
-    //   },
-    //   {
-    //     headers: {
-    //       Authorization: token.data.token
-    //     }
-    //   }
-    // );
-    // console.log(results);
+    const results =await axios.post(
+      "https://bof.profchecksys.com/account/signup",
+      {
+        "firstName": ""+req.body.user_meta.first_name[0],
+        "lastName": ""+req.body.user_meta.last_name[0],
+        "username": ""+req.body.user_meta.first_name[0],
+        "providerName": "",
+        "role": {
+          "id":3,
+          "name": "CUSTOMER",
+          "parentId": null,
+          "parentName": null,
+          "version": 0
+        },
+        "password": ""+req.body.data.user_pass,
+        "userProfile": {
+          "organization": {
+            "id": 70,
+            "name": "N.S.O",
+            "parentId": null,
+            "parentName": null,
+            "version": 0,
+            "logo": ""
+          },
+          "email": ""+req.body.data.user_email,
+          "phone": ""+req.body.user_meta.billing_phone[0],
+          "providerName": ""
+        }
+      },
+      {
+        headers: {
+          Authorization: token.data.token
+        }
+      }
+    );
+    console.log(results);
 
     res.send("ok");
   } catch (error) {
