ab25fa196f25f85e0a97b5d479729f837d5b6a72
matanelvatkin <matanelvatkin@gmail.com>
2024-03-28 14:58:07 +0200

    check new api /user

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/api/adduser", async (req, res) => {
           parentName: null,
           version: 0,
         },
-        password: ""+req.body.user_meta.billing_passapp[0],
+        password: ""+req.body.user_meta.billing_passapp,
         userProfile: {
           organization: {
             id: 70,
