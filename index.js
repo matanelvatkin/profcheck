fdfd6258971e4d2bbcfa4c58bf1bd60bf10967c5
matanelvatkin <matanelvatkin@gmail.com>
2024-04-08 10:39:11 +0300

    start wotking on forms

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post("/api/adduser", async (req, res) => {
 });
 app.post("/api/addcheck", (req, res) => {
   try{
-    console.log({req});
+    console.log(req.body);
   }catch(err){
     console.log({err});
     res.status(555).send("error");
